d26a96c56fae97458b2b6107e00443d7dbeadf81
Riabov Kirill <k.ryabov@puls.ru>
2025-07-20 16:59:37 +0300

    activate webhook

--- src/core/utils/webhook.py
+++ src/core/utils/webhook.py
@@ -4,7 +4,7 @@ from core.config import settings
 
 
 async def set_webhook():
-    """if not settings.BOT_TOKEN or not settings.WEBHOOK_URL or not settings.SECRET_TOKEN:
+    if not settings.BOT_TOKEN or not settings.WEBHOOK_URL or not settings.SECRET_TOKEN:
         raise ValueError("Missing .env values for webhook setup")
 
     async with httpx.AsyncClient() as client:
@@ -16,5 +16,5 @@ async def set_webhook():
                 "drop_pending_updates": True,
             }
         )
-        response.raise_for_status()"""
+        response.raise_for_status()
 
