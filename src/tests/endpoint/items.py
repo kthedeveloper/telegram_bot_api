18df448dcb36dd0e7da09c3c190e6e80035d0eea
Riabov Kirill <k.ryabov@puls.ru>
2025-08-28 20:26:44 +0300

    add tests for endpoint

--- src/tests/endpoint/items.py
+++ src/tests/endpoint/items.py
@@ -36,3 +36,27 @@ async def test_get_items(app):
                                      {'id': 3, 'name': 'Item 3'},
                                      {'id': 4, 'name': 'Item 4'}],
                             'total': 4}
+
+
+@pytest.mark.asyncio
+async def test_get_single_item(app):
+    await asyncio.sleep(3)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("http://127.0.0.1:9999/api/v1/items/1")
+
+        assert r.status_code == 200
+        assert r.json() == {'id': 1, 'name': 'Item 1'}
+
+
+@pytest.mark.asyncio
+async def test_delete_item(app):
+    await asyncio.sleep(3)
+    async with httpx.AsyncClient() as client:
+        r = await client.delete("http://127.0.0.1:9999/api/v1/items/4")
+        assert r.status_code == 200
+
+        r2 = await client.get("http://127.0.0.1:9999/api/v1/items/")
+        assert r2.status_code == 200
+        body = r2.json()
+        assert body["total"] == 3
+        assert {"id": 4, "name": "Item 4"} not in body["data"]
