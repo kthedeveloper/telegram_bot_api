3e9252bc7c5a5e12e10a6f1b2336f86e1a947e4f
Ilya Spirin <ispirin@neuro.net>
2025-08-30 17:06:11 +0300

    unit-tests mocks

--- src/tests/endpoint/items.py
+++ src/tests/endpoint/items.py
@@ -1,32 +1,48 @@
+import os
 import asyncio
 import httpx
-import multiprocessing
 import uvicorn
 import pytest
 import pytest_asyncio
 from testcontainers.postgres import PostgresContainer
 
 from tests.repo.test_user_repo import postgres_container
+import multiprocessing
 
 
 # @pytest.mark.asyncio
 @pytest_asyncio.fixture
 async def app(postgres_container: PostgresContainer):
-    from run import create_app
+    url = postgres_container.get_connection_url(driver='asyncpg')
 
-    app = create_app()
+    def run_app(_url):
+        os.environ['POSTGRES_DSN'] = _url
+        uvicorn.run('run:create_app', host="0.0.0.0", port=9999)
+
     p = multiprocessing.Process(
-        target=lambda: uvicorn.run(app, host="0.0.0.0", port=9999),
-        daemon=True
+        target=run_app,
+        daemon=True,
+        args=(url,),
     )
     p.start()
+
+    async def wait_for_server():
+        while True:
+            try:
+                async with httpx.AsyncClient() as client:
+                    r = await client.get("http://localhost:9999")
+                    if r.status_code == 404:
+                        return  # Server is ready
+            except Exception as e:
+                await asyncio.sleep(0.01)
+
+    await wait_for_server()
 
     return p
 
 
 @pytest.mark.asyncio
 async def test_get_items(app):
-    await asyncio.sleep(5)
     async with httpx.AsyncClient() as client:
         r = await client.get("http://127.0.0.1:9999/api/v1/items/")
 
